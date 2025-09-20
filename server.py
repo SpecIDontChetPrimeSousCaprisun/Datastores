ade4da3de76e2bb66d4fc5b553643d709f1dca42
SpecIDontChetPrimeSousCaprisun <corieux1@gmail.com>
2025-09-20 15:00:42 +0200

    Update server.py

--- server.py
+++ server.py
@@ -39,4 +39,6 @@ def home():
     return "✅ Roblox Datastore API is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    # Local testing only
+    app.run(host="127.0.0.1", port=3000, debug=True)
+
