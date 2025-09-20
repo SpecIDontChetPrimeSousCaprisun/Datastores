e7929e1ae59f202e9d87b8655f6655a85db4a053
SpecIDontChetPrimeSousCaprisun <corieux1@gmail.com>
2025-09-20 14:28:20 +0200

    Update server.py

--- server.py
+++ server.py
@@ -9,12 +9,6 @@ API_KEY = os.getenv("ROBLOX_API_KEY")
 @app.route("/datastores")
 def list_datastores():
     # get universe id from query parameter, fallback to default env variable
-    universe_id = request.args.get("uid", os.getenv("UNIVERSE_ID"))
-    print("Requested uid:", universe_id)
-    print("Request URL:", url)
-    if not universe_id:
-        return jsonify({"error": "No universe ID provided"}), 400
-
     url = f"https://apis.roblox.com/datastores/v1/universes/{universe_id}/universe-datastores"
     headers = {"x-api-key": API_KEY}
 
